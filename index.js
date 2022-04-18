4399838a5b5d017060316a64604d6cce8503cc9e
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-04-18 15:27:21 -0400

    commit tree 6f618b7d9b4225336bbd88f9d2acdf9d2e97a388

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+const submitData = (username, email)=> {
+    fetch(`http://localhost:3000/users`,{
+    method: 'POST',
+    headers: {    
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        },
+    body: JSON.stringify({
+        'name': `${username}`,
+        'email': `${email}`
+    })
+}).then((response)=>response.json())
+.then((newObj) => document.querySelector('body').innerHTML = newObj.id)
+.catch(()=>{
+    document.querySelector('body').innerHTML = error.message
+    // document.querySelector('body').append(message['message'])
+})
+}
